d4cdeabee4c34216a054e978d771be05c26fbf50
JordanDev <ariasjordan943@gmail.com>
2025-06-08 22:34:32 -0400

    Implementación de Cors

--- API/app.js
+++ API/app.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var cors = require('cors'); //cors para permitir peticiones desde el frontend
 var app = express();
 
 // cargar rutas
@@ -10,13 +10,28 @@ var app = express();
 //middlewares
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-//cors
 
 //cargar rutas
 var user_routes = require('./routes/user.routes');
 var follow_routes = require('./routes/follow.routes');
 var publication_routes = require('./routes/publication.routes');
 var message_routes = require('./routes/message.routes');
+
+//cargar cors
+app.use(cors({
+    origin: 'http://localhost:4200',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //rutas
 app.use('/api', user_routes);
